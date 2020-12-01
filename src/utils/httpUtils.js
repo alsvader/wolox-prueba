b91628f3f26d7aa6436a06dbb850f70321fa7e9e
alsvader <alsvader@hotmail.com>
2020-11-30 23:31:59 -0600

    add error handling in httpUtils.js

--- src/utils/httpUtils.js
+++ src/utils/httpUtils.js
@@ -5,12 +5,11 @@ const generateHeaders = () => ({
 const call = async (url, request) => {
   const headers = generateHeaders();
 
-  const response = await fetch(url, { ...request, headers });
-
-  if (!response.ok) {
-    const message = `An error has occured: ${response.status}`;
-    throw new Error(message);
-  }
+  const response = await fetch(url, { ...request, headers })
+    .catch(err => {
+      const message = `An error has occured: ${err.message}`;
+      throw new Error(message);
+    });
 
   return await response.json();
 };
