da420ad2d65fad9b1de9195c75fd0be1c0aad84e
alsvader <alsvader@hotmail.com>
2020-12-03 01:47:22 -0600

    fix error in boundary

--- src/components/ErrorBoundary/ErrorBoundary.jsx
+++ src/components/ErrorBoundary/ErrorBoundary.jsx
@@ -8,7 +8,7 @@ class ErrorBoundary extends Component {
     this.state = { hasError: false };
   }
 
-  static getDerivedStateFromError(error) {
+  static getDerivedStateFromError() {
     return { hasError: true };
   }
 
