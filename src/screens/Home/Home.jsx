7c1bbdff357535d855c83ed1ec40f7bcb434ec56
alsvader <alsvader@hotmail.com>
2020-12-03 22:23:02 -0600

    add benefits section

--- src/screens/Home/Home.jsx
+++ src/screens/Home/Home.jsx
@@ -8,10 +8,49 @@ import imgHero3 from '../../assets/images/img_hero/Ic_ilustra_Hero@3x.png';
 import woloxer from '../../assets/images/woloxer/img_woloxer.png';
 import woloxer2 from '../../assets/images/woloxer/img_woloxer@2x.png';
 import woloxer3 from '../../assets/images/woloxer/img_woloxer@3x.png';
+import hour from '../../assets/images/benefits/Ic_Hour.svg';
+import homeOffice from '../../assets/images/benefits/Ic_HomeOffice.svg';
+import workShop from '../../assets/images/benefits/Ic_Workshops.svg';
+import snacks from '../../assets/images/benefits/Ic_DrinkSnacks.svg';
+import laptop from '../../assets/images/benefits/Ic_laptop.svg';
+import brain from '../../assets/images/benefits/Ic_brain.svg';
 import styles from './styles.module.css';
 
 const Home = () => {
   const { t } = useTranslation();
+
+  const benefits = [
+    {
+      path: hour,
+      alt: 'hour',
+      value: t('flexibleSchedule'),
+    },
+    {
+      path: homeOffice,
+      alt: 'home office',
+      value: t('homeOffice'),
+    },
+    {
+      path: workShop,
+      alt: 'workshop',
+      value: t('trainings'),
+    },
+    {
+      path: snacks,
+      alt: 'snacks',
+      value: t('snackFruit'),
+    },
+    {
+      path: laptop,
+      alt: 'laptop',
+      value: t('weekRemote'),
+    },
+    {
+      path: brain,
+      alt: 'brain',
+      value: t('edgeTech'),
+    },
+  ];
 
   return (
     <>
@@ -59,11 +98,24 @@ const Home = () => {
         <div className={styles.goal}>
           <p>{t('workFor')}</p>
           <p>
-            <span>{t('turn')}</span>
-            <span>{t('ideas')}</span>
-            <span>{t('in')}</span>
+            <span className={styles.blueColor}>{t('turn')}</span>
+            <span className={styles.greenColor}>{t('ideas')}</span>
+            <span>{t('into')}</span>
           </p>
+          <p>{t('products')}</p>
         </div>
+      </div>
+      <div className={[styles.boxContainer, styles.benefits].join(' ')}>
+        <h2>
+          Entre los beneficios que ofrecemos se encuentran
+          <span className={styles.blueColor}> ;)</span>
+        </h2>
+        {benefits.map((item, key) => (
+          <div key={key} className={styles.benefitBox}>
+            <img src={item.path} alt={item.alt} />
+            <p>{item.value}</p>
+          </div>
+        ))}
       </div>
     </>
   );
